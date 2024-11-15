226cb943db976aacb6731d483826c46b145b9b9b
Stefan <stefan.bonney@gmail.com>
2024-11-15 20:07:25 +0200

    tehtävä 1.2

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -1,3 +1,12 @@
+const Part = (props) => {
+  console.log('Part', props)
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Header = (props) => {
   console.log(props)
   return (
@@ -6,18 +15,12 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props)
+  console.log('Content', props)
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   )
 }
