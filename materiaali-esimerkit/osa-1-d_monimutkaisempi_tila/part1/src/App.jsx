9c3878591b59fa5d6324bf66c30642df67589610
Stefan <stefan.bonney@gmail.com>
2024-11-16 02:33:26 +0200

    esimerkit 1d-monimutkaisempi_tila

---
+++ materiaali-esimerkit/osa-1-d_monimutkaisempi_tila/part1/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const History = (props) => {
+  console.log(props)
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <>
+    {console.log('Button props:', { handleClick, text })} {/* Inline log */}
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  </>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+
+    setTotal(left + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
+
+export default App
