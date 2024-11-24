39ee22f7b3dddcbabe80251cd35a3447c3f1368f
Stefan <stefan.bonney@gmail.com>
2024-11-24 16:25:47 +0200

    tehtävä 1.11

--- osa-1/unicafe/src/App.jsx
+++ osa-1/unicafe/src/App.jsx
@@ -11,9 +11,10 @@ const Button = (props) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -38,12 +39,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={`${positivePercentage} %`} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={`${positivePercentage} %`} />
+        </tbody>
+      </table>
     </div>
   );
 };
