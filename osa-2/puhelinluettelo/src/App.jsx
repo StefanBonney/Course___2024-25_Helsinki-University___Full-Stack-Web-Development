16103a742f0c83e78f90f795f2d1b4b58f7aa270
Stefan <stefan.bonney@gmail.com>
2025-01-06 04:00:49 +0200

    tehtävä 2.8

--- osa-2/puhelinluettelo/src/App.jsx
+++ osa-2/puhelinluettelo/src/App.jsx
@@ -1,53 +1,62 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '04012341230' }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  // debug: state after update (post-render)
+  //useEffect(() => {
+  //  console.log('Current state:', { persons, newName, newNumber });
+  //}, [persons, newName, newNumber]);
 
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
 
   const addPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const existingPerson = persons.find(person => person.name === newName);
+    const existingPerson = persons.find(person => person.name === newName)
       if (existingPerson) {
-        alert(`${newName} is already added to phonebook`);
-        return;
+        alert(`${newName} is already added to phonebook`)
+        return
       }
 
-    const newPerson = { name: newName };
+    const newPerson = { 
+      name: newName, 
+      number: newNumber 
+    }
 
-    // debug: log new person & list
-    //console.log('new person:', newPerson);
-    //console.log('new persons list:', [...persons, newPerson]);
+    // debug: state after update (form-submission)
+    //console.log('new person:', newPerson)
+    //console.log('new persons list:', [...persons, newPerson])
 
-    setPersons(persons.concat(newPerson));
-
-    setNewName('');
-  };
-
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
+  }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div> name: <input value={newName} onChange={handleNameChange} /> </div>
+        <div> number: <input value={newNumber} onChange={handleNumberChange} /> </div>
+        <div> <button type="submit">add</button> </div>
       </form>
       <h2>Numbers</h2>
       <ul style={{ listStyle: 'none', padding: 0 }}>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
